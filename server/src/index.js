7f08d6d6c1c4f8196a89873407b234ba96b7db33
Sumukh Atreya <atreya.sumukh@gmail.com>
2025-09-30 22:15:35 +0530

    Basic backend setup

---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import 'dotenv/config';
+import { notFound, errorHandler } from './error-handlers.js';
+
+
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.json({
+        message: 'Greetings!'
+    });
+})
+
+// Not found middleware
+app.use(notFound);
+
+// Error handler
+app.use(errorHandler);
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
