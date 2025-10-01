27d8c8875afc1b06b1f462ce3af5534764499c53
Sumukh Atreya <atreya.sumukh@gmail.com>
2025-10-02 00:16:21 +0530

    Completed OAuth integration for airtable

--- server/src/api/connectors/airtable/index.js
+++ server/src/api/connectors/airtable/index.js
@@ -1,8 +1,12 @@
 import Router from 'express';
 import crypto from 'crypto';
 import Connector from '../../../models/connector.js';
-import { generateSecureString, decryptToken } from '../../../helpers/utils.js';
-
+import {
+  generateSecureString,
+  decryptToken,
+  base64Encode,
+} from '../../../helpers/utils.js';
+import { makeRequest } from '../../../services/http-services.js';
 
 const router = Router();
 
@@ -15,10 +19,14 @@ router.get('/redirect', async (req, res, next) => {
     const state = generateSecureString();
     const scope = 'data.records:read data.records:write schema.bases:read';
     const codeVerifier = generateSecureString();
-    const codeChallenge = crypto.createHash('sha256').update(codeVerifier).digest('base64url');
+    const codeChallenge = crypto
+      .createHash('sha256')
+      .update(codeVerifier)
+      .digest('base64url');
     const codeChallengeMethod = 'S256';
-    // Hacky method for state verification. Ideally should be cached.
-    await Connector.create({ provider: 'airtable', state: state });
+    console.log('State thats being stored', state);
+    // Hacky method for state and code verification. Better solution would be to cache.
+    await Connector.create({ provider: 'airtable', state, codeVerifier });
 
     const airtableRedirectUrl = `${baseUrl}?client_id=${client_id}&redirect_uri=${redirectUrl}&response_type=${responsetype}&state=${state}&scope=${scope}&code_challenge=${codeChallenge}&code_challenge_method=${codeChallengeMethod}`;
 
@@ -30,25 +38,69 @@ router.get('/redirect', async (req, res, next) => {
 });
 
 router.get('/oauth', async (req, res, next) => {
-    try {
-        const { code, state } = req.query;
-        if (!code) {
-            throw new Error('Authorization code not received from airtable url');
-        } else if (!state) {
-            throw new Error('State not received from airtable url');
-        }
-        const airtableConn = await Connector.findOne({ provider: 'airtable' }, { lean: true });
-        const { state: storedState } = airtableConn;
-        if (state !== decryptToken(storedState)) {
-            throw new Error('State strings do not match');
-        }
-        
+  try {
+    const {
+      code,
+      state,
+      code_challenge: codeChallenge,
+      code_challenge_method: codeChallengeMethod,
+      error,
+      error_description: errorDetails,
+    } = req.query;
 
-
-
-    } catch(error) {
-        next(error);
+    if (error) {
+      throw new Error(errorDetails);
     }
+    if (!code) {
+      throw new Error('Authorization code not received from airtable url');
+    }
+    if (!state) {
+      throw new Error('State not received from airtable url');
+    }
+    console.log('State sent in Uri', state);
+    const airtableConn = await Connector.findOne({ provider: 'airtable' });
+    console.log('This is the found airtableConn', airtableConn);
+    const { state: storedState, codeVerifier } = airtableConn;
+    console.log('Stored state', storedState);
+    if (state !== storedState) {
+      throw new Error('State strings do not match');
+    }
+    const reqUrl = 'https://airtable.com/oauth2/v1/token';
+    const credentials = base64Encode(
+      `${process.env.AIRTABLE_CLIENT_ID}:${process.env.AIRTABLE_CLIENT_SECRET}`
+    );
+    const headers = {
+      Authorization: `Basic ${credentials}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    const params = new URLSearchParams({
+      code,
+      client_id: process.env.AIRTABLE_CLIENT_ID,
+      redirect_uri: 'http://localhost:3000/connectors/airtable/oauth',
+      grant_type: 'authorization_code',
+      code_verifier: codeVerifier,
+    });
+
+    const data = await makeRequest(reqUrl, 'POST', headers, params.toString());
+    console.log('This is the received data', data);
+    const {
+      scope,
+      access_token: accessToken,
+      expires_in: expiresIn,
+      refresh_token: refreshToken,
+    } = data;
+
+    airtableConn.accessToken = accessToken;
+    airtableConn.refreshToken = refreshToken;
+    airtableConn.scope = scope;
+    airtableConn.expiresIn = expiresIn;
+
+    await airtableConn.save();
+    res.redirect('http://localhost:3000/')
+  } catch (error) {
+    await Connector.deleteOne({ provider: 'airtable' });
+    next(error);
+  }
 });
 
 export default router;
