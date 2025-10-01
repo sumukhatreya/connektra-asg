e4503b1a232e390a75924db64d049c961f57533c
Sumukh Atreya <atreya.sumukh@gmail.com>
2025-10-01 21:09:06 +0530

    Changes to schema to accomodate state string verification + added pre hook to encrypt access token before saving

--- server/src/models/connector.js
+++ server/src/models/connector.js
@@ -1,36 +1,39 @@
 import mongoose from 'mongoose';
+import { encryptToken } from '../helpers/utils'
 const { Schema, model } = mongoose;
 
 const connectorSchema = new Schema({
     provider: {
         type: String,
         required: true,
-        enum: ['airtable', 'eleven-labs'],
-        index: true
+        enum: ['airtable', 'eleven-labs']
     },
     accessToken: {
         type: String,
-        required: true
     },
     refreshToken: {
         type: String,
-        required: true
     },
     expiresAt: {
         type: Date,
-        required: true
     },
-    isActive: {
-        type: Boolean,
-        default: true
-    },
-    user: {
+    state: {
         type: String,
-        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['success', 'pending', 'failed'],
+        default: 'pending'
     }
 }, { timestamps: true });
 
-// Encryption needs to be added in pre-hook for access and refresh tokens
+connectorSchema.pre('save', function(next) {
+  if (this.isModified('accessToken')) {
+    this.accessToken = encryptToken(this.accessToken);
+  }
+  next();
+});
 
 const Connector = model('Connector', connectorSchema);
 
