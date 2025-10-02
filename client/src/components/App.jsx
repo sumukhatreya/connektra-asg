8e7d1726f20206514e545a5016544bbe73778b39
Sumukh Atreya <atreya.sumukh@gmail.com>
2025-10-02 20:43:05 +0530

    Added basic navbar, along with a minor dir restructure

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import Nav from './Nav'
+import Flow from './Flow'
+
+function App() {
+  return (
+    <div className='app'>
+      <Nav />
+      <div className='content'>
+        {/* <h1>Hello!</h1> */}
+        <Flow />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,10 @@
+
+const Nav = () => {
+    return (
+        <nav className="navbar">
+            <h1>Connektra Asg</h1>
+        </nav>
+    );
+}
+
+export default Nav;
