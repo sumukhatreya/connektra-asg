ce75a034931d2d6ecb0d1db2b4294feeebe5a476
Sumukh Atreya <atreya.sumukh@gmail.com>
2025-10-04 02:31:24 +0530

    manage trigger events in webhook function

---
+++ server/src/api/connectors/airtable/webhook.js
@@ -0,0 +1,38 @@
+import Workflow from '../../../models/workflow.js';
+import { generateAudio } from '../eleven-labs/utils.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+export const webhook = async (req, res, next) => {
+  try {
+    const { service, event, data } = req.body;
+    const directory = path.join(
+      path.dirname(fileURLToPath(import.meta.url)),
+      '..',
+      '..',
+      '..',
+      '..',
+      'audio'
+    );
+    // const directory = path.join(__dirname, '..', '..', '..', '..', 'audio');
+    if (service === 'airtable') {
+      const workflow = await Workflow.findOne({
+        triggerNode: 'airtable',
+        triggerEvent: event,
+      });
+      if (workflow) {
+        const { baseId, tableId, recordId, record } = data;
+        const recordText = JSON.stringify(record).replace(/[^a-zA-Z0-9 ]/g, '');
+        const audioFileName = `${baseId}-${tableId}-${recordId}.mp3`;
+        await generateAudio(audioFileName, directory, recordText);
+        res.status(201).json({ message: `Audio file created: ${audioFileName}`});
+      } else {
+        throw new Error('Workflow not found');
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// export default webhook;
